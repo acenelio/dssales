e9ec47cf0065352c4e0d5743e088aa1db91d70ea
Washington Soares <washingtonudi@gmail.com>
2021-07-19 19:38:30 -0300

    Add Header component

---
+++ front-web/src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+function Header() {
+  return (
+    <header className="main-header-container">
+      <h1 className="main-header-title">DS Sales</h1>
+    </header>
+  );
+}
+
+export default Header;
